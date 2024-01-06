3336f42cd9ce09aeb6903a4872dbaf82df1cfec5
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-01-06 15:04:01 +0530

    visibility issues fixed

--- src/components/Introblog.jsx
+++ src/components/Introblog.jsx
@@ -13,24 +13,34 @@ export const Introblog = () => {
       <heading>Featured Blogs</heading>
     <div className="blogs">
     <BlogDisplay
-      heading = "The Newest Blog"
+      heading = "Navigating the Maze: A Beginner's Guide to Mindful Living"
       imgurl = "https://picsum.photos/400/600.jpg"
       time = "5"
     />
     <BlogDisplay
-      heading = "The Newest Blog"
+      heading = "Tech Trends 2024: Unleashing the Future of Innovation"
       imgurl = "https://picsum.photos/800/400.jpg"
       time = "7"
     />
     <BlogDisplay
-      heading = "The Newest Blog"
+      heading = "Culinary Adventures: Exploring Global Flavors in Your Kitchen"
       imgurl = "https://picsum.photos/1200/900.jpg"
-      time = "5"
+      time = "2"
     />
     <BlogDisplay
-      heading = "The Newest Blog"
+      heading = "Beyond the Pages: Book Club Chronicles and Literary Musings"
       imgurl = "https://picsum.photos/400.jpg"
-      time = "5"
+      time = "12"
+    />
+    <BlogDisplay
+      heading = "Beyond the Pages: Book Club Chronicles and Literary Musings"
+      imgurl = "https://picsum.photos/400.jpg"
+      time = "12"
+    />
+    <BlogDisplay
+      heading = "Beyond the Pages: Book Club Chronicles and Literary Musings"
+      imgurl = "https://picsum.photos/400.jpg"
+      time = "12"
     />
 
 
